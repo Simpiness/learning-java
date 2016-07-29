9c92a59f8132be75d3aa5801247f9e13fe30c977
zhousen <zhousen1011@gmail.com>
2016-07-29 15:01:37 +0800

    Java项目练习

---
+++ src/main/java/cn/wostore/jlearing/App.java
@@ -0,0 +1,13 @@
+package cn.wostore.jlearing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
